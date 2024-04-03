c647a6f3c2a506640eb847f055a1bdcc17558070
pyjavathon <ayeonyun391@gmail.com>
2024-04-03 09:55:43 +0900

    모달창까지 완성

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,44 +1,20 @@
 import React from "react";
 import { createSearchParams, useNavigate, useParams, useSearchParams } from "react-router-dom";
+import ReadComponent from "../../components/todo/ReadComponent";
 
-function ReadPage(props){
-
-    const navigate = useNavigate()
+const ReadPage = () =>{
 
     const {tno} = useParams()
 
-    const [queryParams] = useSearchParams()
-
-    const page = queryParams.get('page') ? parseInt(queryParams.get('page')) : 1
-    const size = queryParams.get('size') ? parseInt(queryParams.get('size')) : 10
-
-    const queryStr = createSearchParams({page:page,size:size}).toString()
-
-    console.log(tno)
-
-    const moveToModify = (tno) => {
-        navigate({pathname:`/todo/modify/${tno}`,
-        search: queryStr
-        })
-    }
-
-    const moveToList = () => {
-        navigate({
-            pathname: `/todo/list`,
-            search: queryStr
-        })
-    }
-
     return (
 
-        <div className={'text-3xl'}>
-            Todo Read Page {tno}
-
-            <div>
-                <button onClick={() => moveToModify(tno)}>Test Modify</button>
-                <button onClick={moveToList}>Test List</button>
+       <div className="font-extrabold w-full bg-white mt-6">
+            <div className="text-2xl">
+                Todo Read Page Component {tno}
             </div>
-        </div>
+
+            <ReadComponent tno={tno}/>
+       </div>
 
     );
 }
